8dee6a7509e3ee0eb67ba3caa02c8809632b8294
Denis Avdeev <davdeev@me.com>
2025-12-22 16:00:50 +0500

    [a1rt1] Recreate Demo from Lecture 2

---
+++ CodeBreaker/MatchMarkers.swift
@@ -0,0 +1,45 @@
+//
+//  MatchMarkers.swift
+//  CodeBreaker
+//
+//  Created by Denis Avdeev on 22.12.2025.
+//
+
+import SwiftUI
+
+enum Match {
+    case exact
+    case inexact
+    case noMatch
+}
+
+struct MatchMarkers: View {
+    var matches: [Match]
+    
+    var body: some View {
+        VStack {
+            HStack {
+                matchMarker(peg: 0)
+                matchMarker(peg: 1)
+            }
+            HStack {
+                matchMarker(peg: 2)
+                matchMarker(peg: 3)
+            }
+        }
+    }
+    
+    func matchMarker(peg: Int) -> some View {
+        let exactCount: Int = matches.count(where: { match in match == .exact })
+        let foundCount: Int = matches.count(where: { match in match != .noMatch })
+        
+        return Circle()
+            .fill(exactCount > peg ? Color.primary : .clear)
+            .strokeBorder(foundCount > peg ? Color.primary : .clear, lineWidth: 2)
+            .aspectRatio(contentMode: .fit)
+    }
+}
+
+#Preview {
+    MatchMarkers(matches: [.exact, .inexact, .noMatch])
+}
